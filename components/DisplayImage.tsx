8b2cca2e26236fb95d18c86e09b6a2774082269a
Cliv-create <66003937+Cliv-create@users.noreply.github.com>
2025-06-20 16:33:33 +0300

    Added optional attributes parameter for DisplayImage component. Updated documentation.

--- components/DisplayImage.tsx
+++ components/DisplayImage.tsx
@@ -4,17 +4,37 @@ interface DisplayImageProps {
     idName? : string;
     src: string;
     alt: string;
+    attributes?: HTMLAttribute[];
+}
+
+interface HTMLAttribute {
+    name: string;
+    value: string | number | boolean;
 }
 
 /**
- * Returns img tag with src and alt parameters set.
- * @param props array of properties, accepts "src", "alt", "idName" as strings
+ * Returns <img> HTML tag with id, src, alt and provided attribute list set.
+ * @param idName sets "id" for <img>
+ * @param src sets "src" for <img> with provided string
+ * @param alt sets "alt" for <img> with provided string
+ * @param attributes accepts HTMLAttribute array (see HTMLAttribute interface), converts it into Record
  * @constructor
  */
-export function DisplayImage({ idName, src, alt } : DisplayImageProps) {
+export function DisplayImage({ idName, src, alt, attributes = [] } : DisplayImageProps) {
+    const imgAttributes = attributes.reduce((acc, attr) => {
+        acc[attr.name] = attr.value;
+        return acc;
+    }, {} as Record<string, string | number | boolean>);
+
     return (
         <>
-            <img id={idName} className='display-image' src={src} alt={alt} />
+            <img
+                id={idName}
+                className='display-image'
+                src={src}
+                alt={alt}
+                {...imgAttributes}
+            />
         </>
     )
 }
