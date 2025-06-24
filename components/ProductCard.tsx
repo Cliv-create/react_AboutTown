165dff4f13d26828002c85bfb4cdf3947ca3cd9e
Cliv-create <66003937+Cliv-create@users.noreply.github.com>
2025-06-24 20:20:41 +0300

    Edited return code for ProductCard.tsx Added new .css for ProductCard.css Added documentation for ProductCard.tsx Added spacing.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -9,18 +9,21 @@ interface ProductCardProps {
     attributes?: HTMLAttribute[];
 }
 
+
 interface HTMLAttribute {
     name: string;
     value: string | number | boolean;
 }
+
+
 // TODO: Add a "Add to card" button. Add a shopping_cart_id parameter if shopping carts will be implemented to link button.
 /**
- *
- * @param idName
- * @param title
- * @param text
- * @param price
- * @param attributes
+ * Returns a "product card" with given parameters. To efficiently utilize the component, place the ProductCard object/s inside a container with className "product-grid". The cards will be placed in this container as a grid. Container will be edited with the CSS from ProductCard.css
+ * @param idName String. Set's "id" attribute for the component.
+ * @param title String. Displays product title.
+ * @param text String. Displays product description.
+ * @param price Number. Displays price.
+ * @param attributes Array of HTMLAttribute. Converts into Record[] and is used in parent div with "product-card" class.
  * @constructor
  */
 export function ProductCard({ idName, title, text, price, attributes = [] } : ProductCardProps) {
@@ -46,23 +49,34 @@ export function ProductCard({ idName, title, text, price, attributes = [] } : Pr
     }, {} as Record<string, string | number | boolean>);
 
     return (
-        <>
-            <div
-                className="product-card"
-                id={idName}
-                {...productCardAttributes}
-            >
-                <div className='product-card-top-box'></div>
-                <div
-                    className="product-card-main-box"
-                >
-                    <p>{title}</p>
-                    <p>{text}</p>
-                </div>
-                <div className='product-card-bottom-box'>
-                    <p>{price}</p>
+        <div
+             className="product-card"
+             id={idName}
+             {...productCardAttributes}
+        >
+            <div className="product-card-content">
+                <div className="product-card-grid">
+                    <div>
+                        <h2 className="product-title">{title}</h2>
+                        <p className="product-text">{text}</p>
+                    </div>
+                    <div className="product-price-container">
+                        <p>
+                            <span className="product-price">${price}</span>
+                            <span className="product-price-suffix"> /usd </span>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </>
+            <div className="product-card-footer">
+                <a
+                    href="#"
+                    className="product-card-button"
+                    aria-describedby="product-card-select-button"
+                >
+                    Get started
+                </a>
+            </div>
+        </div>
     )
 }
